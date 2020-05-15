554f9547356c743ec7dc199e17e51763793ed60f
Christopher Best <bestchris@gmail.com>
2020-05-15 19:21:04 -0400

    Style lid with border and tile spacing

--- src/Lid.js
+++ src/Lid.js
@@ -4,6 +4,8 @@ import Tile from './Tile';
 import entity from './entity';
 import { LID } from './constants';
 
+import './Lid.css';
+
 export default ({ game: { tiles } }) => <div className="lid">
   {
     entity(tiles).filter(({ location }) => location === LID).map(tile => <Tile key={tile.key} tile={tile} />)
